a467436f7da9b2bf1b70fd160a10869d4bb66a18
Rushank Savant <rushanksavant34@gmail.com>
2022-02-26 19:54:42 +0530

    +Token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,10 +4,13 @@ async function main() {
 
   const Greeter = await hre.ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, Hardhat!"); // "Hello, Hardhat!" is input to constructor() defined in Greeter.sol 
-
   await greeter.deployed();
-
   console.log("Greeter deployed to:", greeter.address);
+
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy(); // no input in the constructor
+  await token.deployed();
+  console.log("Token deployed to:", token.address);
 }
 
 main()
--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import { useState } from 'react';
 import { ethers } from 'ethers';
 import './App.css';
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+import Token from './artifacts/contracts/Token.sol/Token.json';
 
 const greeterAddress = "0x5357C6422e9edb9800e9a0abB44E29e701aC2f95"; // contract address
+const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // contract address
+
 
 function App() {
 
   const [greeting, setGreetingValue] = useState('');
+  const [userAccount, setUserAccount] = useState('');
+  const [amount, setAmount] = useState(0)
 
   async function requestAccount() { // will make user navigate to metamast 
     await window.ethereum.request({ method: 'eth_requestAccounts' }); // will prompt user to conect metamask
@@ -45,15 +50,51 @@ function App() {
       fetchGreeting() // will log out the new value
     }
   }
+  // -------------------------------------------------------------------------------------------------------------------------------------------
+  async function getBalance() {
+    if (typeof window.ethereum !== 'undefined') { // checks if user has matamask installed or not
+      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' }) // account of the user
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider) // instance of contract
+
+      const balance = await contract.balanceOf(account); // balanceOf() defined in Token.sol
+      console.log("Balance: ", balance.toString());
+    }
+  }
+
+  async function sendCoins() {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+
+      const transaction = await contract.transfer(userAccount, amount);
+      await transaction.wait();
+
+      console.log(`${amount} Coins successfully sent to ${userAccount}`);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+
         <button onClick={fetchGreeting}>Fetch Greeting</button>
         <button onClick={setGreeting}>Set Greeting</button>
         <input
           onChange={event => setGreetingValue(event.target.value)}
           value={greeting}
           placeholder="Set Greeting" />
+        {/* -------------------------------------------------------------------------------------------------------------------------------------------------------- */}
+        <br />
+        <button onClick={getBalance}>Get Balance</button>
+        <button onClick={sendCoins}>Send Coins</button>
+        <input onChange={event => setUserAccount(event.target.value)} placeholder="Account ID" />
+        <input onChange={event => setAmount(event.target.value)} placeholder="Amount" />
+
       </header>
     </div>
   );
